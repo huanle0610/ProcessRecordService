62c2749269db09e3bdd7ea2c8151f5be9e2f3642
aladin <huanle0610@gmail.com>
2016-07-25 07:01:36 +0800

    添加项目文件。

---
+++ MyProcessService/Service1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace MyProcessService
+{
+    public partial class Service1 : ServiceBase
+    {
+        private int eventId;
+
+        public Service1()
+        {
+            InitializeComponent();
+            if (!EventLog.SourceExists("MySource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "MySource", "MyNewLog");
+            }
+            eventLog1.Source = "MySource";
+            eventLog1.Log = "MyNewLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("In OnStart");
+
+            // Set up a timer to trigger every minute.
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 60000; // 60 seconds
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+
+        public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            // TODO: Insert monitoring activities here.
+            eventLog1.WriteEntry("Monitoring the System", EventLogEntryType.Information, eventId++);
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("In OnStop");
+        }
+    }
+}
